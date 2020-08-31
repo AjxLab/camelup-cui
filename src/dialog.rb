21f2f490458320df9b65178b868a2c5484c8ec24
averak <abe12@mccc.jp>
2020-09-01 03:20:14 +0900

    [update] display dialog!

--- src/dialog.rb
+++ src/dialog.rb
@@ -16,20 +16,30 @@ class Dialog
 
   public
 
+  def message(*msg, center: false, start_x: 0, start_y: 0)
+    raise ArgumentError unless start_y >= 0 && start_y + msg.length <= height
+    raise ArgumentError unless start_x >= 0 && start_x <= width
+
+    print "\e[#{start_point[1] + start_y}B\e[#{start_point[0]}C"
+
+    msg.map! { |line| ' ' * ((width - length(line)) / 2) + line } if center
+    msg.map! { |line| ' ' * start_x + "#{line}\e[#{length(line) + start_x}D\e[B" }
+    print "\e[40m#{msg.join}\e[0m"
+
+    print "\e[#{start_point[1] + msg.length}A\e[#{start_point[0]}D"
+  end
+
   def draw_frame
-    print "\e[#{start_point[1]}B\e[#{start_point[0]}C"
-    print frame
-    print "\e[#{start_point[1] + height}A\e[#{start_point[0]}D"
+    message *frame
   end
 
   private
 
   def frame
-    result = ''
+    result = []
 
     height.times do |_y|
-      result += "\e[40m \e[0m" * width
-      result += "\e[B\e[#{width}D"
+      result << "\e[40m \e[0m" * width
     end
 
     result
@@ -40,5 +50,10 @@ class Dialog
       `tput cols`.to_i / 2 - width / 2,
       `tput lines`.to_i / 2 - height / 2
     ]
+  end
+
+  def length(str)
+    str = str.gsub /\e\[\d*[ABCDm]/, ''
+    str.length + str.chars.reject(&:ascii_only?).length
   end
 end
--- src/main.rb
+++ src/main.rb
@@ -6,5 +6,14 @@ system('clear')
 
 @dialog = Dialog.instance
 @dialog.draw_frame
+@dialog.message 'あなたのターンです', center: true
+@dialog.message(
+  'サイコロを振る',
+  '> 馬券を獲得する',
+  'オアシスを配置する',
+  '蜃気楼を配置する',
+  start_x: 10,
+  start_y: 2
+)
 
 sleep 3
